24801b3240ef9c036eac77c7a9e267f4c3e2d487
Tomas-Dean <athene227@gmail.com>
2020-01-13 13:56:43 +0500

    designed answers schema

--- app/Question.php
+++ app/Question.php
@@ -24,7 +24,7 @@ class Question extends Model
     }
 
     public function getStatusAttribute(){
-        if( $this->answers>0 ){
+        if( $this->answers_count>0 ){
             if($this->best_answer_id){
                 return "answered-accepted";
             }
@@ -34,6 +34,9 @@ class Question extends Model
     }
     public function getBodyHtmlAttribute(){
         return \Parsedown::instance()->text($this->body);
+    }
+    public function answers(){
+        return $this->hasMany(Answer::class);
     }
 
 }
