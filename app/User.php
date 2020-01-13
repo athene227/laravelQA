f33467661b239f37dbcf0e8d338dfd4a4fd76d0d
Tomas-Dean <athene227@gmail.com>
2020-01-13 16:43:56 +0500

    answers displayed with gravatars

--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
         // return route("questions.show", $this->id);
         return'#';
     }
+    public function getAvatarAttribute(){
+        $email = $this->email;
+        $size = 32;
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -16,6 +16,51 @@
 
                 <div class="card-body">
                 {!! $question->body_html !!}
+                <div class="float-right">
+                    <div class="media">
+                        <a href="{{$question->user->url}}" class="pr-2">
+                          <img src="{{$question->user->avatar}}">  
+                        </a>
+                        <div class="media-body mt-1">
+                        <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                        </div>
+                        </div>
+                    <span class="text-muted  mt-2"> Answered {{$question->created_at}} </span>
+                    
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                    <h2>{{$question->answers_count. " ". str_plural('Answer', $question->answers_count)}}</h2>
+                    </div>
+                    <hr>
+                    @foreach ($question->answers as $answer)
+                        <div class="media">
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="float-right">
+                                    <div class="media">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                          <img src="{{$answer->user->avatar}}">  
+                                        </a>
+                                        <div class="media-body mt-1">
+                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                        </div>
+                                        </div>
+                                    <span class="text-muted  mt-2"> Answered {{$answer->created_at}} </span>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
                 </div>
             </div>
         </div>
